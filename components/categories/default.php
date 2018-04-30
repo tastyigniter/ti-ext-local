d6104fcf9dd06df5746906c49f6be4c95f77b937
SamPoyigi <sam@sampoyigi.com>
2018-04-30 04:05:39 +0100

    FIx show all categories link

--- components/categories/default.php
+++ components/categories/default.php
@@ -1,9 +1,12 @@
 <div class="panel panel-default">
     <div class="list-group list-group-root">
         <?php if (strlen($selectedCategory)) { ?>
-        <a class="list-group-item text-danger" href="<?= restaurant_url($menusPage) ?>">
-            <i class="fa fa-times"></i>&nbsp;&nbsp;<?= lang('sampoyigi.local::default.text_clear'); ?>
-        </a>
+            <a
+                class="list-group-item text-danger"
+                href="<?= page_url('local/menus', ['category' => null]) ?>"
+            >
+                <i class="fa fa-times"></i>&nbsp;&nbsp;<?= lang('sampoyigi.local::default.text_clear'); ?>
+            </a>
         <?php } ?>
 
         <?= partial('@items', ['categories' => $categories]); ?>
