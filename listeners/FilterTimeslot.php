86a885ed073503d7231639f04187fd2c24e3487b
Ryan Mitchell <ryan@thoughtcollective.com>
2020-08-17 08:34:28 +0100

    Bring logic into new approach

--- listeners/FilterTimeslot.php
+++ listeners/FilterTimeslot.php
@@ -4,13 +4,19 @@ namespace Igniter\Local\Listeners;
 
 use Admin\Models\Orders_model;
 use Carbon\Carbon;
+use DateTime;
 use Igniter\Flame\Location\Models\AbstractLocation;
-use Igniter\Local\Facades\Location;
+use Igniter\Local\Classes\Location;
+use Igniter\Local\Facades\Location as LocationFacade;
+use Igniter\Flame\Traits\EventEmitter;
 use Illuminate\Contracts\Events\Dispatcher;
 
 class FilterTimeslot
 {
-    protected static $ordersCache;
+    use EventEmitter;
+
+    protected static $ordersCache = [];
+    protected static $scheduleCache = [];
 
     public function subscribe(Dispatcher $dispatcher)
     {
@@ -22,95 +28,73 @@ class FilterTimeslot
         // Skip if the working schedule is not for delivery or pickup
         if ($workingSchedule->getType() == AbstractLocation::OPENING)
             return;
+            
+        // allow the timeslot selection logic to be overwritten by extensions
+        if ($event = $this->fireSystemEvent('igniter.local.timeslotValid', [$timeslot])) {
+            return $event;
+        }
+            
+        $dateString = Carbon::parse($timeslot)->toDateString();
+            
+        $ordersOnThisDay = $this->getOrders($dateString);
+                
+        $location = new Location(LocationFacade::getId());
+        $limitTimeslots = $this->getSchedule($location, $dateString);
 
-        $ordersOnThisDay = $this->getOrders($timeslot->toDateString());
-        // rest of the logic here
+        foreach ($limitTimeslots as $limitDate => $limitHoursArray)
+        {
+            if ($limitDate == $dateString)
+            {
+                foreach ($limitHoursArray as $limitHours)
+                {
+                    $datetime = Carbon::parse($timeslot);
+                    $startTime = Carbon::parse($limitHours);
+                    $endTime = Carbon::parse($limitHours)->addMinutes($location->getModel()->getOption('limit_orders_interval'));
+
+                    if ($datetime->between($startTime, $endTime))
+                    {
+                        $orderCount = $ordersOnThisDay->filter(function ($order) use ($startTime, $endTime) {
+                            $orderTime = Carbon::createFromFormat('Y-m-d H:i:s', $order->order_date->format('Y-m-d').' '.$order->order_time);
+
+                            return $orderTime->between(
+                                $startTime,
+                                $endTime
+                            );
+                        });
+
+                        return $orderCount->count() < $location->getModel()->getOption('limit_orders_count');
+                    }
+                }
+            }
+        }
+        
+        return false;
+
     }
 
     protected function getOrders($date)
     {
-        if (!is_null(self::$ordersCache))
-            return self::$ordersCache;
+        if (array_has(self::$ordersCache, $date))
+            return self::$ordersCache[$date];
 
         $result = Orders_model::where('order_date', $date)
-            ->where('location_id', Location::getId())
+            ->where('location_id', LocationFacade::getId())
             ->whereIn('status_id', setting('processing_order_status', []))
             ->select(['order_time', 'order_date'])
             ->pluck('order_time', 'order_date');
 
-        return self::$ordersCache = $result;
+        return self::$ordersCache[$date] = $result;
     }
-
-    public function scheduleTimeslot()
+    
+    protected function getSchedule($location, $date)
     {
-        // cache repeated calls
-        if (!isset($this->scheduleCache)) {
-            $this->scheduleCache = [
-                'type' => '',
-                'cache' => [],
-            ];
-        }
+        if (array_has(self::$scheduleCache, $date))
+            return self::$scheduleCache[$date];
 
-        if ($this->orderType() == $this->scheduleCache['type']) {
-            return $this->scheduleCache['cache'];
-        }
-
-//        $schedule = $this->workingSchedule($this->orderType())->getTimeslot(
-//            $this->orderTimeInterval(), null, $this->orderLeadTime()
-//        );
-
-        $self = $this;
-        $schedule->each(function ($timeslots, $dateKey) use ($self, $schedule) {
-            $limitTimeslots = $self->getModel()->getOption('limit_orders') ? $this->workingSchedule($this::OPENING)->getTimeslot(
-                $this->getModel()->getOption('limit_orders_interval'), new DateTime($dateKey), 0
-            )->toArray() : [];
-
-            $ordersOnThisDay = $self->getModel()->getOption('limit_orders') ? Orders_model::where([
-                ['location_id', '=', $this->getId()],
-                ['order_date', '=', $dateKey],
-                ['status_id', '!=', '0'],
-            ])->select(['order_date', 'order_time'])->get() : [];
-
-            $timeslots = $timeslots->filter(function ($item, $key) use ($self, $limitTimeslots, $dateKey, $ordersOnThisDay) {
-                // allow the following logic to be overwritten by extensions
-                if ($event = $self->fireSystemEvent('igniter.local.timeslotValid', [$item, $key, $dateKey])) {
-                    return $event;
-                }
-
-                if (!$self->getModel()->getOption('limit_orders')) return TRUE;
-
-                foreach ($limitTimeslots as $limitDate => $limitHoursArray) {
-                    if ($limitDate == $dateKey) {
-                        foreach ($limitHoursArray as $limitHours) {
-                            $datetime = Carbon::parse($item);
-                            $startTime = Carbon::parse($limitHours);
-                            $endTime = Carbon::parse($limitHours)->addMinutes($this->getModel()->getOption('limit_orders_interval'));
-
-                            if ($datetime->between($startTime, $endTime)) {
-                                $orderCount = $ordersOnThisDay->filter(function ($order) use ($startTime, $endTime) {
-                                    $orderTime = Carbon::createFromFormat('Y-m-d H:i:s', $order->order_date->format('Y-m-d').' '.$order->order_time);
-
-                                    return $orderTime->between(
-                                        $startTime,
-                                        $endTime
-                                    );
-                                });
-
-                                return $orderCount->count() < $self->getModel()->getOption('limit_orders_count');
-                            }
-                        }
-                    }
-                }
-
-                return FALSE;
-            });
-
-            $schedule->put($dateKey, $timeslots);
-        });
-
-        $this->scheduleCache['type'] = $this->orderType();
-        $this->scheduleCache['cache'] = $schedule;
-
-        return $schedule;
+        $schedule = $location->getModel()->getOption('limit_orders') ? $location->workingSchedule($location::OPENING)->getTimeslot(
+            $location->getModel()->getOption('limit_orders_interval'), new DateTime($date), 0
+        )->toArray() : [];
+        
+        return self::$scheduleCache[$date] = $schedule;
     }
 }
