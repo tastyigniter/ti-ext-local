33a6ef76a1f652baa636f880be1b380c071f9069
Ryan Mitchell <ryan@thoughtcollective.com>
2021-01-25 20:46:10 +0000

    Updated logic

--- components/LocalBox.php
+++ components/LocalBox.php
@@ -243,16 +243,14 @@ class LocalBox extends \System\Classes\BaseComponent
 
         $locationHasOrderTypes = $locationCurrent->availableOrderTypes();
         $defaultOrderType = $this->property('defaultOrderType', Locations_model::DELIVERY);
-        if (!in_array($defaultOrderType, $locationHasOrderTypes))
-            return;
+        if (!in_array($defaultOrderType, $locationHasOrderTypes)) {
+            if (!count($locationHasOrderTypes))
+                return;
 
-        if (!$this->location->getSession('orderType'))
-            $this->location->updateOrderType($defaultOrderType);
-
-        // make sure location supports default
-        if (!in_array($defaultOrderType, $locationHasOrderTypes))
             $defaultOrderType = array_shift($locationHasOrderTypes);
+        }
 
-        $this->location->updateOrderType($defaultOrderType);
+        if (!$sessionOrder = $this->location->getSession('orderType') OR !in_array($sessionOrder, $locationCurrent->availableOrderTypes()))
+            $this->location->updateOrderType($defaultOrderType);
     }
 }
