189928f739c43bf76a423324e38d966ae53ef81c
Ryan Mitchell <ryan@thoughtcollective.com>
2021-02-04 20:20:58 +0000

    Move to pie charts

--- views/reviews/lists/averages.blade.php
+++ views/reviews/lists/averages.blade.php
@@ -1,5 +1,8 @@
 @if($recordCount = $this->records->count())
-@php $hints = \Igniter\Local\Models\Reviews_model::make()->getRatingOptions(); @endphp
+@php 
+    $hints = \Igniter\Local\Models\Reviews_model::make()->getRatingOptions(); 
+    $pieColors = ['#4DB6AC', '#64B5F6', '#BA68C8'];
+@endphp
 <div class="row px-3 mt-1 mb-3">
     @foreach (['quality', 'service', 'delivery'] as $ratingType)
     <div class="col">
@@ -8,13 +11,34 @@
                 <h5>@lang('igniter.local::default.reviews.label_'.$ratingType)</h5>
                 @for ($rating = 5; $rating > 0; $rating--)
                     @php 
-                        $filteredRecords = $this->records->where($ratingType, $rating);
-                        $ratingHint = $hints[$rating] ?? $rating;
+                        $chartData = [
+                			'datasets' => [
+                        		[
+                					'data' => [],
+                					'backgroundColor' => [],
+                				],
+                			],
+                			'labels' => [],
+                		];
+                        
+                        for ($rating=5; $rating>0; $rating--) {
+                            $chartData['datasets'][0]['data'][] = $this->records->where($ratingType, $rating)->count(); 
+                            $chartData['datasets'][0]['backgroundColor'][] = $pieColors[$rating % count($pieColors)];
+                            $chartData['labels'][] = $hints[$rating] ?? $rating;
+                        }
                     @endphp
-                <div class="progress mt-1" style="height:24px;">
-                    <div class="progress-bar" role="progressbar" style="width: {{ 100 * $filteredRecords->count() / $recordCount }}%"><span class="h6 pt-2">{{ $ratingHint }} ({{ $filteredRecords->count() }})</span></div>
-                </div>
                 @endfor
+                <div
+                    class="chart-container"
+                    data-control="review-chart"
+                >
+                    <div class="chart-canvas">
+            			<textarea style="display:none;">{!! json_encode($chartData) !!}</textarea>
+                        <canvas
+            				style="width: 100%; height: 200px"
+                        ></canvas>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
