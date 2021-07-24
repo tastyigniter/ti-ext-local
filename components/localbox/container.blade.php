e586d6e7ff35d11d384b505b8fec86b3e7652720
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2021-07-24 13:26:56 +0100

    Remove address search bar if order type is not delivery

--- components/localbox/container.blade.php
+++ components/localbox/container.blade.php
@@ -1,20 +1,22 @@
 <div id="local-box">
-    <div class="panel local-search">
-        <div class="panel-body">
-            <div class="row">
-                @if (!$hideSearch)
-                    <div class="col-sm-12">
-                        @partial('@searchbar')
-                    </div>
+    @if ($location->orderTypeIsDelivery())
+        <div class="panel local-search">
+            <div class="panel-body">
+                <div class="row">
+                    @if (!$hideSearch)
+                        <div class="col-sm-12">
+                            @partial('@searchbar')
+                        </div>
+                    @endif
+                </div>
+                @if ($location->requiresUserPosition()
+                    AND $location->userPosition()->hasCoordinates()
+                    AND !$location->checkDeliveryCoverage())
+                    <span class="help-block">@lang('igniter.local::default.text_delivery_coverage')</span>
                 @endif
             </div>
-            @if ($location->requiresUserPosition()
-                AND $location->userPosition()->hasCoordinates()
-                AND !$location->checkDeliveryCoverage())
-                <span class="help-block">@lang('igniter.local::default.text_delivery_coverage')</span>
-            @endif
         </div>
-    </div>
+    @endif
 
     @partial($__SELF__.'::default')
 
