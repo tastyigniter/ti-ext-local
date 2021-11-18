6cbc6d1516f913ad42cd104fecd2b1c2fa55ba17
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2021-11-18 20:07:57 +0000

    Add database foreign key constraints
    Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

---
+++ database/migrations/2021_11_18_010000_make_primary_key_bigint_all_tables.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Igniter\Local\Database\Migrations;
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class MakePrimaryKeyBigintAllTables extends Migration
+{
+    public function up()
+    {
+        Schema::table('igniter_reviews', function (Blueprint $table) {
+            $table->unsignedBigInteger('review_id', TRUE)->change();
+            $table->unsignedBigInteger('customer_id')->nullable()->change();
+            $table->unsignedBigInteger('sale_id')->nullable()->change();
+            $table->unsignedBigInteger('location_id')->nullable()->change();
+        });
+    }
+
+    public function down()
+    {
+    }
+}
