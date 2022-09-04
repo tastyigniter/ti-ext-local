ba320c67eff70af209e426e4e9b49d86b362fed7
Philip <philipxyc@live.com>
2022-09-04 15:37:46 +0000

    Fix: cannot import menu category field

--- models/MenuImport.php
+++ models/MenuImport.php
@@ -94,7 +94,7 @@ class MenuImport extends ImportModel
             }
             else {
                 $newCategory = Categories_model::firstOrCreate(['name' => $name]);
-                $ids[] = $this->categoryNameCache[$name] = $newCategory->id;
+                $ids[] = $this->categoryNameCache[$name] = $newCategory->category_id;
             }
         }
 
