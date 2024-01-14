9ac48dadd2a3f79e03d3db98bb58c7068deeb557
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2024-01-14 23:50:30 +0000

    Minor refactor
    Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

--- database/migrations/2021_09_06_010000_add_timestamps_to_reviews.php
+++ database/migrations/2021_09_06_010000_add_timestamps_to_reviews.php
@@ -4,10 +4,13 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up()
     {
+        if (!Schema::hasColumn('igniter_reviews', 'date_added')) {
+            return;
+        }
+
         Schema::table('igniter_reviews', function (Blueprint $table) {
             $table->timestamp('date_added')->change();
             $table->renameColumn('date_added', 'created_at');
