626ac5322a6021f5f958b7dcc7260f5fdfb7c1eb
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2024-05-23 15:08:37 +0100

    Add factory for review model
    Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

---
+++ src/Database/Factories/ReviewFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Igniter\Local\Database\Factories;
+
+use Igniter\Flame\Database\Factories\Factory;
+
+class ReviewFactory extends Factory
+{
+    protected $model = \Igniter\Local\Models\Review::class;
+
+    public function definition(): array
+    {
+        return [
+            'location_id' => $this->faker->numberBetween(1, 200),
+            'customer_id' => $this->faker->numberBetween(1, 200),
+            'sale_id' => $this->faker->numberBetween(1, 200),
+            'sale_type' => 'orders',
+            'author' => $this->faker->name,
+            'quality' => $this->faker->numberBetween(0, 6),
+            'delivery' => $this->faker->numberBetween(0, 6),
+            'service' => $this->faker->numberBetween(0, 6),
+            'review_text' => $this->faker->text(),
+            'review_status' => 1,
+        ];
+    }
+}
