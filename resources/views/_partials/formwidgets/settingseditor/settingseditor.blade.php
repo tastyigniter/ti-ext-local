eac892cbd734f412877307a304a3e01c4d74aca5
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2025-07-09 00:45:12 +0100

    style: enhance accordion item styling
    Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

--- resources/views/_partials/formwidgets/settingseditor/settingseditor.blade.php
+++ resources/views/_partials/formwidgets/settingseditor/settingseditor.blade.php
@@ -1,20 +1,20 @@
 <div class="accordion" id="accordionSettings">
     @foreach($settings as $group => $setting)
-        <div class="accordion-item">
+        <div class="accordion-item border rounded-3 mb-2">
             @if((!request()->has('general') && $setting->code === 'checkout') || request()->get('general') == $setting->code)
                 <h2 class="accordion-header">
-                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse{{ ucfirst($setting->code) }}" aria-expanded="true" aria-controls="collapse{{ ucfirst($setting->code) }}">
+                    <button class="accordion-button border-0 rounded-3 shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapse{{ ucfirst($setting->code) }}" aria-expanded="true" aria-controls="collapse{{ ucfirst($setting->code) }}">
                         @lang($setting->label)
                     </button>
                 </h2>
-                <div id="collapse{{ ucfirst($setting->code) }}" class="accordion-collapse collapse show" data-bs-parent="#accordionSettings">
+                <div id="collapse{{ ucfirst($setting->code) }}" class="accordion-collapse border-top collapse show" data-bs-parent="#accordionSettings">
                     <div class="accordion-body">
                         {!! $this->onLoadRecord($setting->code) !!}
                     </div>
                 </div>
             @else
                 <h2 class="accordion-header">
-                    <a class="accordion-button collapsed"  href="{{ admin_url('locations/settings/' . $this->model->id . '?general=' . $setting->code) }}" aria-expanded="false" aria-controls="collapse{{ ucfirst($setting->code) }}">
+                    <a class="accordion-button border-0 rounded-3 collapsed"  href="{{ admin_url('locations/settings/' . $this->model->id . '?general=' . $setting->code) }}" aria-expanded="false" aria-controls="collapse{{ ucfirst($setting->code) }}">
                         @lang($setting->label)
                     </a>
                 </h2>
