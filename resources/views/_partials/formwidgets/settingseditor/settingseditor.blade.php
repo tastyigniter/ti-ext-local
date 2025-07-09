b03ecacebe90bf93d9f2d5b4c295be621d5d31a8
Obinna Elvis Okechukwu <dreamor47@gmail.com>
2025-07-09 22:26:28 +0100

    fix bug when you select another settings and change setting url format and logic

--- resources/views/_partials/formwidgets/settingseditor/settingseditor.blade.php
+++ resources/views/_partials/formwidgets/settingseditor/settingseditor.blade.php
@@ -1,7 +1,8 @@
 <div class="accordion" id="accordionSettings">
     @foreach($settings as $group => $setting)
         <div class="accordion-item border rounded-3 mb-2">
-            @if((!request()->has('general') && $setting->code === 'checkout') || request()->get('general') == $setting->code)
+            @php($currentSegment = collect(request()->segments())->last())
+            @if((!str_contains($currentSegment, 'general-') && $loop->first) || $currentSegment === 'general-' . $setting->code)
                 <h2 class="accordion-header">
                     <button class="accordion-button border-0 rounded-3 shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapse{{ ucfirst($setting->code) }}" aria-expanded="true" aria-controls="collapse{{ ucfirst($setting->code) }}">
                         @lang($setting->label)
@@ -14,7 +15,7 @@
                 </div>
             @else
                 <h2 class="accordion-header">
-                    <a class="accordion-button border-0 rounded-3 collapsed"  href="{{ admin_url('locations/settings/' . $this->model->id . '?general=' . $setting->code) }}" aria-expanded="false" aria-controls="collapse{{ ucfirst($setting->code) }}">
+                    <a class="accordion-button border-0 rounded-3 collapsed"  href="{{ admin_url('locations/settings/' . $this->model->location_id . '/general-' . $setting->code) }}" aria-expanded="false" aria-controls="collapse{{ ucfirst($setting->code) }}">
                         @lang($setting->label)
                     </a>
                 </h2>
